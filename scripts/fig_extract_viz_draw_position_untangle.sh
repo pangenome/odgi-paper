6f6eaaf6d8162feb75b4d2d88c2d1280a192755a
AndreaGuarracino <andreaguarracino@outlook.com>
2021-10-29 10:30:29 +0200

    odgi untangle visualization

--- scripts/fig_extract_viz_draw_position_untangle.sh
+++ scripts/fig_extract_viz_draw_position_untangle.sh
@@ -56,3 +56,52 @@ odgi position -i ${filename_chr6_gfa}.C4.sorted.og -E chr6.C4.gtf -t 16 > ${file
 # Convert to GFA for Bandage
 odgi view -i ${filename_chr6_gfa}.C4.sorted.og -g > ${filename_chr6_gfa}.C4.sorted.gfa
 
+
+# Untangle
+
+f=${filename_chr6_gfa}.C4.sorted.og
+( echo query.name query.start query.end ref.name ref.start ref.end score inv self.cov x|
+  tr ' ' '\t'; odgi untangle -i $f -r $(odgi paths -i $f -L | grep chm13) -t 16 -m 256 |
+  bedtools sort -i - ) | awk '$8 == "-" { x=$6; $6=$5; $5=x; } { print }' |
+  tr ' ' '\t'   >$f.untangle.bed.tsv
+
+# cat $f.untangle.bed.tsv | grep '^chm13\|^grch38\|^HG00438\|^HG0107\|^HG01952'
+
+# R
+library(ggplot2)
+x <- read.table('/home/guarracino/Desktop/pangenomics/odgi_pub/chr6.pan.fa.a2fb268.4030258.d9f1245.smooth.gfa.C4.sorted.og.untangle.bed.tsv', sep = '\t', header = T, comment.char="$")
+x$query.name <- gsub(":.*","",x$query.name)
+
+ggplot(
+  subset(x, query.name %in% c(
+    ##"chm13#chr6:31825251-31908851",
+    #"grch38#chr6:31972046-32055647",
+    ##"HG00438#1#JAHBCB010000040.1:24269348-24320210",
+    #"HG00438#2#JAHBCA010000042.1:24398231-24449090",
+    ##"HG01071#1#JAHBCF010000017.1:706180-783405",
+    #"HG01071#2#JAHBCE010000076.1:7794179-7897781",
+    #"HG01952#1#JAHAME010000044.1:28380191-28451052",
+    #"HG01952#2#JAHAMD010000016.1:31974838-32052065",
+
+    #"chm13#chr6",
+    "grch38#chr6",
+    #"HG00438#1#JAHBCB010000040.1",
+    "HG00438#2#JAHBCA010000042.1",
+    #"HG01071#1#JAHBCF010000017.1",
+    "HG01071#2#JAHBCE010000076.1",
+    "HG01952#1#JAHAME010000044.1",
+    "HG01952#2#JAHAMD010000016.1"
+    )
+  ), aes(x=query.start, xend=query.end, y=ref.start, yend=ref.end)) +
+    geom_segment() +
+    facet_grid(. ~ query.name) +
+    coord_fixed() +
+    theme(
+      text = element_text(size = 12.6),
+      axis.text.x = element_text(size = 12, angle = 90),
+      axis.text.y = element_text(size = 12),
+    )  +
+      xlab("Query start") +
+      ylab("Reference start")
+ggsave('chr6.C4.sorted.untangle.bed.subset.tsv.pdf', width = 32, height = 8,  units = "cm", dpi = 300,  bg = "transparent")
+
