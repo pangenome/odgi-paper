11da47ecaa909256c3fb1bdcacf7e44e4f040a7a
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2021-11-18 16:10:58 +0100

    include 0 on the y-axis for Fig. 2

--- scripts/time.R
+++ scripts/time.R
@@ -9,9 +9,9 @@ args <- commandArgs(trailingOnly = T)
 times_csv <- args[1]
 times_pdf <- args[2]
 
-# times_csv <- "/home/heumos/Downloads/odgi_paper/MHC/mean_build_time.csv"
+# times_csv <- "/home/heumos/git/odgi-paper/data/mean_build_time.csv"
 
-times <- read.delim(times_csv)
+times <- read.delim(times_csv, sep = ",", as.is = T, header = T)
 
 summary(times)
 
@@ -20,7 +20,8 @@ times$threads <- factor(times$threads, levels=unique(times$threads))
 p <- ggplot(times, aes(x=threads, y=time, group=threads)) +
     geom_violin() +
     #geom_beeswarm(size=0.5) +
-    labs(x = "number of threads", y = "time in seconds")
+    labs(x = "number of threads", y = "time in seconds") +
+    expand_limits(x = 0, y = 0)
 #pdf(times_pdf, height = 2.5, width = 3)
 ggsave(times_pdf, device = cairo_pdf, height = 2.5, width = 3)
 knitr::plot_crop(times_pdf)
